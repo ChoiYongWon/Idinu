cd39ac1732d34853bb6af2d5431e02ef29416225
ChoiYongWon <yongwon0824@naver.com>
2021-04-08 17:30:51 +0900

    :zap:Add js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Yongwon on 2021-04-08
+ * Email: yongwon0824@naver.com
+ */
+
+const customVar = {
+    isTop : true
+}
+
+const headerFixed = () => {
+
+    let y = document.documentElement.scrollTop
+    let header = document.querySelector(".idinu-header")
+    if(y > 0 && !header.classList.contains("header-fixed")){
+        header.classList.add("header-fixed")
+    }else if(y == 0 && header.classList.contains("header-fixed")){
+        header.classList.remove("header-fixed")
+    }
+}
+
+window.onload = () => {
+    headerFixed()
+    document.addEventListener("scroll", (e)=>{
+        headerFixed()
+        let y = window.innerHeight;
+        let animation_component = document.querySelectorAll(".animation")
+        for(let element of animation_component){
+            if(element.getBoundingClientRect().top + 300 < y){
+                if(!element.classList.contains("show")){
+                    element.classList.add("show")
+                }
+            }
+
+        }
+        
+    })
+}
